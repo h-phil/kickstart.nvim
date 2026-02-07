c44d254a01c2b0f0478b5fa79ee0f1e58d648e11
phil <inciteinc@protonmail.com>
2026-02-07 12:57:13 +0100

    custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'h-phil/kickstart.nvim',
+  config = function()
+    -- center for jumps
+    vim.keymap.set({ 'n', 'v' }, '<C-d>', '<C-d>zz', { silent = true })
+    vim.keymap.set({ 'n', 'v' }, '<C-u>', '<C-u>zz', { silent = true })
+    -- center for jumps
+    -- https://vi.stackexchange.com/questions/37287/in-nvim-can-i-map-tab-without-removing-c-i-as-jump-forward
+    vim.keymap.set({ 'n', 'v' }, '<C-I>', '<C-I>')
+    vim.keymap.set({ 'n', 'v' }, '<Tab>', '<Nop>')
+  end,
+}
